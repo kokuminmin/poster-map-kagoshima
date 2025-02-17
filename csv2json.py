32f165da64121c09be019e59fe792177c0d30036
Yuko Kimura <toranekoland@gmail.com>
2025-02-17 23:00:09 +0900

    Update

--- csv2json.py
+++ csv2json.py
@@ -9,8 +9,8 @@ df = pd.read_csv(csv_file_path)
 
 print(df)
 
-#    df.groupby(['area_block'])
 result = (
+    df.groupby(['area_block'])
     .apply(lambda group:{
         "area_name": group.name[0],
         "area_block": group.name[1]
@@ -18,8 +18,12 @@ result = (
     .tolist()
 )
 
-with open(json_file_path, mode='w', encoding='utf-8') as json_file:
-    json.dump(result, json_file, ensure_ascii=False, indent=4)
+print(result)
 
-print(f"JSON 生成: {json_file_path}")
+exit
+
+#with open(json_file_path, mode='w', encoding='utf-8') as json_file:
+#    json.dump(result, json_file, ensure_ascii=False, indent=4)
+#
+#print(f"JSON 生成: {json_file_path}")
 
