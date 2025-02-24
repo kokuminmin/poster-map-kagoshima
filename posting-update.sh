d3bf8bdbc96070121c90db35f17d21304cadf393
Yuko Kimura <toranekoland@gmail.com>
2025-02-24 22:33:10 +0900

    ポスティングデータアップデートShell

--- posting-update.sh
+++ posting-update.sh
@@ -9,7 +9,7 @@ GCS_URL="https://script.google.com/macros/s/AKfycbyjKhFSnV0WAizpm7bi19AAdHtJBQoq
 # メイン処理
 cd "$BASE_DIR"
 
-#git pull
+git pull
 
 # posting map data download
 curl -sL "$GCS_URL?sheetName=postingmapdata" > "$DATA_DIR/conquerlist.csv"
@@ -21,13 +21,13 @@ python3 bin/conquercsv2json_small.py "$DATA_DIR/conquerlist.csv" "$DATA_DIR/conq
 python3 bin/summarize_areatotal.py "$DATA_DIR/conquerareatotal.json"
 
 # Git 操作
-#git add -N .
+git add -N .
 
-#if ! git diff --exit-code --quiet
-#then
-#    git add .
-#    git commit -m "Update"
-#    git push
+if ! git diff --exit-code --quiet
+then
+    git add .
+    git commit -m "Posting Data Update"
+    git push
 #    #source .env
 #    #npx netlify-cli deploy --prod --message "Deploy" --dir=./public --auth $NETLIFY_AUTH_TOKEN
-#fi
+fi
