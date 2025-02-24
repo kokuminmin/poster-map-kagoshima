e08a58bf3d21c79df98d343688607cb61433ce1f
Yuko Kimura <toranekoland@gmail.com>
2025-02-24 20:46:42 +0900

    自動更新に向けて微調整

--- postermap-update.sh
+++ postermap-update.sh
@@ -2,14 +2,14 @@
 set -euo pipefail
 
 # 定数の定義
-BASE_DIR=~/poster-map/poster-map-kagoshima/
+BASE_DIR=~/poster-map/poster-map-kagoshima-release/
 DATA_DIR=public/data
 GCS_URL="https://script.google.com/macros/s/AKfycbyjKhFSnV0WAizpm7bi19AAdHtJBQoqHsiETTk7zHhgX8muK_xVW7O_zcJYVFfKK48/exec"
 
 # メイン処理
 cd "$BASE_DIR"
 
-#git pull
+git pull
 
 # 掲示板ピンマップデータをダウンロード
 echo "データをダウンロード"
@@ -28,14 +28,14 @@ echo "残り数カウント"
 python3 bin/summarize_progress_absolute.py "$DATA_DIR/all.csv" "$DATA_DIR/arealist.csv" "$DATA_DIR/summary_absolute.json"
 
 # Git 操作
-#git add -N .
+git add -N .
 
-#if ! git diff --exit-code --quiet
-#then
-#    git add .
-#    git commit -m "Update"
-#    git push
+if ! git diff --exit-code --quiet
+then
+    git add .
+    git commit -m "Data Update"
+    git push
 #    #source .env
 #    #npx netlify-cli deploy --prod --message "Deploy" --dir=./public --auth $NETLIFY_AUTH_TOKEN
-#fi
+fi
 
